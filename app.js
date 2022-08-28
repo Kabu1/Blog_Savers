ed24a5e01ef73c23f64bcc0fe4cd6cf9115ef1d5
Kabu1 <47355999+Kabu1@users.noreply.github.com>
2022-08-28 17:54:38 +0300

    Feature/homepage set up (#1)
    * add 404 and create blog html page
    
    * add dynamic title using ejs
    
    * display blogs

--- app.js
+++ app.js
@@ -10,22 +10,34 @@ app.set('view engine', 'ejs');
  app.listen(3000);
 
  app.get('/', (req, res) => {
-     res.render('index');
+     const blogs = [
+         {title: 'Loving Nodejs', snippet: 'lorem Ipsum'},
+         {title: 'Loving Expressjs', snippet: 'lorem Ipsum'},
+         {title: 'Loving ejs', snippet: 'lorem Ipsum'},
+
+     ];
+     res.render('index', {title: 'Home', blogs});
     //  res.send('<p>HOME PAGE</p>');
     // res.sendFile('./views/index.html', {root: __dirname});
     // console.log('root', {root: __dirname});
  });
  app.get('/about', (req, res) => {
     // res.send('<p>ABOUT PAGE</p>');
-    res.sendFile('./views/about.html', {root: __dirname});
+    // res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', {title: 'About'});
+
 });
 //redirects
-app.get('/about-us', (req, res) => {
-    // res.send('<p>ABOUT PAGE</p>');
-    res.redirect('/about');
-});
+// app.get('/about-us', (req, res) => {
+//     // res.send('<p>ABOUT PAGE</p>');
+//     res.redirect('/about');
+// });
+app.get('/blogs/create', (req, res)=>{
+    res.render('create', {title: 'Create New Blog'});
+})
 //404 page
 app.use((req, res) => {
     // res.send('<p>ABOUT PAGE</p>');
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    // res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404', {title: '404'});
 });
