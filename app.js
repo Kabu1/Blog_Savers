32dc6b986b50d9ff3af4a5394949806a83e66867
Kabu1 <bensonmk99@gmail.com>
2022-08-28 16:46:06 +0300

    add dynamic title using ejs

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
  app.listen(3000);
 
  app.get('/', (req, res) => {
-     res.render('index');
+     res.render('index', {title: 'Home'});
     //  res.send('<p>HOME PAGE</p>');
     // res.sendFile('./views/index.html', {root: __dirname});
     // console.log('root', {root: __dirname});
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs');
  app.get('/about', (req, res) => {
     // res.send('<p>ABOUT PAGE</p>');
     // res.sendFile('./views/about.html', {root: __dirname});
-    res.render('about');
+    res.render('about', {title: 'About'});
 
 });
 //redirects
@@ -27,11 +27,11 @@ app.set('view engine', 'ejs');
 //     res.redirect('/about');
 // });
 app.get('/blogs/create', (req, res)=>{
-    res.render('create')
+    res.render('create', {title: 'Create New Blog'});
 })
 //404 page
 app.use((req, res) => {
     // res.send('<p>ABOUT PAGE</p>');
     // res.status(404).sendFile('./views/404.html', {root: __dirname});
-    res.status(404).render('404');
+    res.status(404).render('404', {title: '404'});
 });
